eaaa49779997ebca50f5963fdf7391628160ff25
Stepan Ageev <ageevstepan94@gmail.com>
2017-11-05 15:06:05 +0300

    1.3. JavaDoc. Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,11 +1,17 @@
 package ru.job4j;
 
+/**
+* Класс Calculate
+*
+* @author Stepan Ageev (flowryder@mail.ru)
+* @since 04.11.2017
+*/
 public class Calculate {
-    static {
-        System.out.println("Hello World!");
-    }
-
+    /**
+	* Метод main, выводит на консоль "Hello World!"
+	* @param args
+	*/
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Stepan Ageev (flowryder@mail.ru)
+* @version $Id$
+* @since 04.11.2017
+*/
+package ru.job4j;
