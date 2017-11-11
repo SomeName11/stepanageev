8bb01ff939c3e17045437af709c8780670a61487
Stepan Ageev <ageevstepan94@gmail.com>
2017-11-11 13:18:28 +0300

    4.2. Создать программу вычисляющую факторил[#30583] Исправление

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -4,12 +4,11 @@ public class Factorial {
     public int calc(int n) {
         int result = 1;
         if (n == 0) {
-            return result;
         } else {
             for (int i = 1; i <= n; i++) {
                 result *= i;
             }
-            return result;
         }
+        return result;
     }
 }
